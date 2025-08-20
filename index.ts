4b6f40338a85ad3c8e485a64c6e6d4cee3be0de7
Paul Rowe <paul.rowe@method.com>
2025-08-20 11:02:56 -0600

    completed tutorial

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { runAiSupportAgent, runMemoryAgent } from "./agents";
+
+async function main() {
+  const query = "What is agent parallelization?";
+  const chunks = await runMemoryAgent(query);
+  const completion = await runAiSupportAgent({
+    chunks,
+    query,
+  });
+
+  console.log("Completion:", completion);
+}
+
+main();
